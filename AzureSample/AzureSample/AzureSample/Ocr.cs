e4bf48adba3c46e3f1975092d2febe83e0eba010
Yoshito Tabuchi @ MBP-Xamarin <ytabuchi.xlsoft@gmail.com>
2017-07-21 09:14:07 +0900

    add translator

---
+++ AzureSample/AzureSample/AzureSample/Ocr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace AzureSample
+{
+    public class Ocr
+    {
+        public Ocr()
+        {
+        }
+    }
+}
+
+
+//https://southeastasia.api.cognitive.microsoft.com/vision/v1.0
---
+++ AzureSample/AzureSample/AzureSample/Translate.cs
@@ -0,0 +1,29 @@
+﻿/* This class get a result of Translator API
+ * http://docs.microsofttranslator.com/text-translate.html#!/default/get_Translate
+ */
+using System;
+using System.Net.Http;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace AzureSample
+{
+    public class Translate
+    {
+		const string url = "https://api.microsofttranslator.com/v2/http.svc/Translate";
+
+		public static async Task<string> TranslateTextAsync(string rowText)
+		{
+			var token = await AuthenticationToken.GetBearerTokenAsync(Secrets.TranslatorApiKey);
+
+			using (var client = new HttpClient())
+			{
+				var sendUri = $"{url}?appid={token}&text={rowText}&from=ja&to=en&category=generalnn";
+				var stream = await client.GetStreamAsync(sendUri);
+				var doc = XElement.Load(stream);
+
+				return doc.Value;
+			}
+		}
+    }
+}
