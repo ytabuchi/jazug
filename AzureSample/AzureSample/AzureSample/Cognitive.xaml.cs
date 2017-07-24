981ad451a515ae37ee98d78a3389b1bc83d4818a
Yoshito Tabuchi @ MBP-Xamarin <ytabuchi.xlsoft@gmail.com>
2017-07-24 10:13:26 +0900

    add media plugin

--- AzureSample/AzureSample/AzureSample/Cognitive.xaml.cs
+++ AzureSample/AzureSample/AzureSample/Cognitive.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plugin.Media;
 using Xamarin.Forms;
 
 namespace AzureSample
@@ -22,5 +22,47 @@ namespace AzureSample
             var resEn = await Translate.TranslateTextAsync(resJa);
             resEnLabel.Text = resEn;
         }
+
+        async void PhotoClicked(object sender, EventArgs e)
+        {
+			await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", ":( No camera available.", "OK");
+				return;
+			}
+
+			var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+			{
+				Directory = "Sample",
+				Name = "test.jpg"
+			});
+
+			if (file == null)
+				return;
+
+			//await DisplayAlert("File Location", file.Path, "OK");
+
+			image.Source = ImageSource.FromStream(() =>
+			{
+				var stream = file.GetStream();
+				return stream;
+			});
+
+			var resJa = await Ocr.DoOcrStreamAsync(file.GetStream());
+			//var resJa = await Ocr.MakeOCRRequest(file.Path);
+			resJaLabel.Text = resJa;
+
+			var resEn = await Translate.TranslateTextAsync(resJa);
+			resEnLabel.Text = resEn;
+
+
+			//file.Dispose();
+
+			//or:
+			//image.Source = ImageSource.FromFile(file.Path);
+			//image.Dispose();
+		}
     }
 }
--- AzureSample/AzureSample/AzureSample/Ocr.cs
+++ AzureSample/AzureSample/AzureSample/Ocr.cs
@@ -22,17 +22,17 @@ namespace AzureSample
         {
             using (var client = new HttpClient())
             {
-                try
-                {
-					// ヘッダーでAPIキーを付与して日本語でComputer Vision APIに投げる
-					client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Secrets.ComputerVisionApiKey);
-					var sendUri = $"{uriBase}?language=ja&detectOrientation=true";
+				// ヘッダーでAPIキーを付与して日本語でComputer Vision APIに投げる
+				client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Secrets.ComputerVisionApiKey);
+				var sendUri = $"{uriBase}?language=ja&detectOrientation=true";
 
+				try
+                {
                     // 画像URLをJSONコンテントとしてPOSTする
-					var content = new StringContent("{\"url\":\"" + imageUri + "\"}");
-					content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-					var response = await client.PostAsync(sendUri, content);
-					response.EnsureSuccessStatusCode();
+                    var content = new StringContent("{\"url\":\"" + imageUri + "\"}");
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    var response = await client.PostAsync(sendUri, content);
+                    response.EnsureSuccessStatusCode();
 
                     // 取得したJSONから読み取り結果をリターンする
                     var contentString = await response.Content.ReadAsStringAsync();
@@ -48,103 +48,79 @@ namespace AzureSample
                     }
 
                     return sb.ToString();
-				}
+                }
                 catch (Exception ex)
                 {
                     System.Diagnostics.Debug.WriteLine(ex.InnerException);
+					return "couldn't recognize.";
                 }
-                return "couldn't recognize.";
-			}
-        }
-
-        /// <summary>
-        /// Gets the text visible in the specified image file by using the Computer Vision REST API.
-        /// </summary>
-        /// <param name="imageFilePath">The image file.</param>
-        static async Task<string> MakeOCRRequest(string imageFilePath)
-        {
-            HttpClient client = new HttpClient();
-
-            // Request headers.
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Secrets.ComputerVisionApiKey);
-
-            // Request parameters.
-            string requestParameters = "language=ja&detectOrientation=true";
-
-            // Assemble the URI for the REST API Call.
-            string uri = uriBase + "?" + requestParameters;
-
-            HttpResponseMessage response;
-
-            // Request body. Posts a locally stored JPEG image.
-            byte[] byteData = GetImageAsByteArray(imageFilePath);
-
-            using (ByteArrayContent content = new ByteArrayContent(byteData))
-            {
-                // This example uses content type "application/octet-stream".
-                // The other content types you can use are "application/json" and "multipart/form-data".
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-
-                // Execute the REST API call.
-                response = await client.PostAsync(uri, content);
-
-                // Get the JSON response.
-                string contentString = await response.Content.ReadAsStringAsync();
-
-				var ocrResultString = JsonConvert.DeserializeObject<OcrResult>(contentString);
-
-				var sb = new StringBuilder();
-				foreach (var line in ocrResultString.regions[0].lines)
-				{
-					foreach (var word in line.words)
-					{
-						sb.Append(word.text);
-					}
-				}
-
-                return sb.ToString();
             }
         }
 
-
-        /// <summary>
-        /// Returns the contents of the specified file as a byte array.
-        /// </summary>
-        /// <param name="imageFilePath">The image file to read.</param>
-        /// <returns>The byte array of the image data.</returns>
-        static byte[] GetImageAsByteArray(string imageFilePath)
+        public static async Task<string> DoOcrStreamAsync(Stream imageStream)
         {
-            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
-            BinaryReader binaryReader = new BinaryReader(fileStream);
-            return binaryReader.ReadBytes((int)fileStream.Length);
+            using (var client = new HttpClient())
+            {
+                // ヘッダーとパラメーターを付与したリクエストを作成
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Secrets.ComputerVisionApiKey);
+                var sendUri = $"{uriBase}?language=ja&detectOrientation=true";
+
+                try
+                {
+                    // StreamをコンテントとしてPOSTする
+                    var content = new StreamContent(imageStream);
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    var response = await client.PostAsync(sendUri, content);
+                    response.EnsureSuccessStatusCode();
+
+                    // Get the JSON response.
+                    string contentString = await response.Content.ReadAsStringAsync();
+
+                    var ocrResultString = JsonConvert.DeserializeObject<OcrResult>(contentString);
+
+                    var sb = new StringBuilder();
+                    foreach (var line in ocrResultString.regions[0].lines)
+                    {
+                        foreach (var word in line.words)
+                        {
+                            sb.Append(word.text);
+                        }
+                    }
+
+                    return sb.ToString();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                    return "couldn't recognize.";
+                }
+            }
         }
-
-
     }
 
     public class OcrResult
     {
-		public class Word
-		{
-			public string boundingBox { get; set; }
-			public string text { get; set; }
-		}
+        public class Word
+        {
+            public string boundingBox { get; set; }
+            public string text { get; set; }
+        }
 
-		public class Line
-		{
-			public string boundingBox { get; set; }
-			public List<Word> words { get; set; }
-		}
+        public class Line
+        {
+            public string boundingBox { get; set; }
+            public List<Word> words { get; set; }
+        }
 
-		public class Region
-		{
-			public string boundingBox { get; set; }
-			public List<Line> lines { get; set; }
-		}
+        public class Region
+        {
+            public string boundingBox { get; set; }
+            public List<Line> lines { get; set; }
+        }
 
-		public string language { get; set; }
-		public double textAngle { get; set; }
-		public string orientation { get; set; }
-		public List<Region> regions { get; set; }
+        public string language { get; set; }
+        public double textAngle { get; set; }
+        public string orientation { get; set; }
+        public List<Region> regions { get; set; }
     }
 }
